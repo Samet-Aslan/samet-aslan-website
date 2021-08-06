49d4cc500c413b7e8d56c5ed8a218280fb0e3b64
Samet-Aslan <me@samet-aslan.de>
2021-08-06 14:44:41 +0200

    added a flipbox to aavatar

--- src/components/MyAvatar.js
+++ src/components/MyAvatar.js
@@ -1,23 +1,35 @@
 import React from "react";
 import Avatar from "avataaars";
+import me from "../images/me.jpg"
 
 function MyAvatar(){
 
     return(
-            <Avatar
-                avatarStyle='Circle'
-                topType='ShortHairShortFlat'
-                accessoriesType='Blank'
-                hairColor='BrownDark'
-                facialHairType='BeardLight'
-                facialHairColor='BrownDark'
-                clotheType='Hoodie'
-                clotheColor='Heather'
-                eyeType='Happy'
-                eyebrowType='DefaultNatural'
-                mouthType='Smile'
-                skinColor='Light'
-            />
+
+        <div class="flip-box">
+            <div class="flip-box-inner">
+                <div class="flip-box-front">
+                    <Avatar
+                        avatarStyle='Circle'
+                        topType='ShortHairShortFlat'
+                        accessoriesType='Blank'
+                        hairColor='BrownDark'
+                        facialHairType='BeardLight'
+                        facialHairColor='BrownDark'
+                        clotheType='Hoodie'
+                        clotheColor='Heather'
+                        eyeType='Happy'
+                        eyebrowType='DefaultNatural'
+                        mouthType='Smile'
+                        skinColor='Light'
+                    />
+    </div>
+    <div class="flip-box-back">
+        <img src={me}></img>
+    </div>
+  </div>
+</div>
+            
     );
 }
 export default MyAvatar;
