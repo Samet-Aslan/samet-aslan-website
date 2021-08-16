19ba6798737638589d9db4afbc4c7a5449412dbc
samet-aslan <me@samet-aslan.de>
2021-08-16 09:27:49 +0200

    added associated link to project title

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -15,7 +15,7 @@ function ProjectItem(props){
         <div className="columns is-vcentered" id={rev}>
             <div className="column is-half has-text-left">
                 <h1 className="title is-spaced is-size-4-desktop is-size-4-tablet is-size-5-mobile" id="project-title">
-                    {props.name}
+                    <a href={props.url} target="_blank" rel="noreferrer">{props.name}</a>
                 </h1>
                 <h2 className="subtitle is-size-5-desktop is-size-6-tablet is-size-6-mobile">
                     {props.description}
